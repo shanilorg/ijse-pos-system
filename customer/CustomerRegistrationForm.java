0dae99645c3b40d3edeed1aaf2b5d00887a309ad
shanil <shanilmiranda24@gmail.com>
2021-08-07 12:49:28 +0530

    closes #1; Added customer interface with related functionalities

---
+++ customer/CustomerRegistrationForm.java
@@ -0,0 +1,20 @@
+//*---------------------------------------------------------------------------------------------
+// *  Copyright (c) Shanil Erosh. All rights reserved.
+// *  Licensed under the MIT License. See License.txt in the project root for license information.
+// *--------------------------------------------------------------------------------------------*/
+package customerregistrationform;
+
+/**
+ *
+ * @author shanilmiranda
+ */
+public class CustomerRegistrationForm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
